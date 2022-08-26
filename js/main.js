32cf1c1154c096bc82dd5b51010ed17f2a94a335
LeandroCanuto <leandrocanutto@hotmail.com>
2022-08-26 13:35:08 -0300

    funções anonimas e declaradas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert('oi')
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click",function (){
+
+    console.log("Cliquei no robo")
+})
+
+function dizOI() {
+    console.log("oi")
+
+}
