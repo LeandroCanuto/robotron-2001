44dd8337d496f181b109070221edbbf0f1156b2a
LeandroCanuto <leandrocanutto@hotmail.com>
2022-08-26 20:10:31 -0300

    ativado soma e subtracao do braço

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const robotron = document.querySelector("#robotron")
 
-robotron.addEventListener("click",function (){
+//Armazenar elementos HTML em variáveis
+
+robotron.addEventListener("click", () =>{
 
     console.log("Cliquei no robo")
 })
@@ -9,3 +11,17 @@ function dizOI() {
     console.log("oi")
 
 }
+dizOI()
+
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+//----------------------------------------------------------
+
+somar.addEventListener("click",(evento) =>{
+    braco.value = parseInt( braco.value) + 1
+})
+
+subtrair.addEventListener("click",(evento) =>{
+    braco.value = parseInt( braco.value) - 1
+})
