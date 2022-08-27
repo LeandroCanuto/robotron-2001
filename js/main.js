43d8911e9911321c3417644a5a0f1766714bb584
LeandroCanuto <leandrocanutto@hotmail.com>
2022-08-27 17:10:53 -0300

    Melhorada soma e subtracao

--- js/main.js
+++ js/main.js
@@ -16,12 +16,30 @@ dizOI()
 const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
+const controle = document.querySelectorAll(".controle-ajuste")
+
+
 //----------------------------------------------------------
 
-somar.addEventListener("click",(evento) =>{
-    braco.value = parseInt( braco.value) + 1
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+        //console.log(evento.target.textContent)
+    })
 })
+    
 
-subtrair.addEventListener("click",(evento) =>{
-    braco.value = parseInt( braco.value) - 1
-})
+
+
+function manipulaDados(operacao){
+
+    if(operacao === "-"){
+       braco.value = parseInt(braco.value) - 1
+       console.log("menos")
+    }else{
+        braco.value = parseInt(braco.value) + 1 
+        console.log("mais")
+
+    }
+
+}
